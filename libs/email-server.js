c52a31c0ad202cf2bedbbc3461552286b45f61ab
Kwang Kim <kkim@ondeck.com>
2016-05-04 19:56:09 -0400

    cleanup

--- libs/email-server.js
+++ libs/email-server.js
@@ -6,20 +6,9 @@ mailin.start({
   disableWebhook: true // Disable the webhook posting.
 });
 
-mailin.on('authorizeUser', function (connection, username, password, done) {
-  if (username == "johnsmith" && password == "mysecret") {
-    done(null, true);
-  } else {
-    done(new Error("Unauthorized!"), false);
-  }
-});
-
 mailin.on('message', function (connection, data, content) {
-  console.log(data);
   var sentence = data.text.split("\n")[0];
-  console.log('sentence: ' + sentence);
   var first = sentence.split(" ")[0];
-  console.log('first: ' + first);
   if (first != 'note') {
     balancedRequest(sentence, function (err, res) {
       if (err) console.log(err);
