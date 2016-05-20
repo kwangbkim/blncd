1284aa4b26366d6761e8531f784426e49a30ab92
Kwang Kim <kkim@ondeck.com>
2016-05-20 16:21:38 -0400

    remove unused modules

--- libs/cli.js
+++
@@ -1,7 +0,0 @@
-var request = require('./balanced-request');
-
-var cleanedRequest = process.argv[2].replace(/['"]+/g, '');
-request(cleanedRequest, function (err, res) {
-  if (err) console.log(err);
-  console.log(res.body);
-});
