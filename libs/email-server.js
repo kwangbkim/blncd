0006c2a0042895b1accccd83bdc1b4a3df5ec9c1
Kwang Kim <kkim@ondeck.com>
2016-05-21 13:39:57 -0400

    allow gets from mail

--- libs/email-server.js
+++ libs/email-server.js
@@ -1,18 +1,26 @@
 var mailin = require('mailin'),
-  balancedRequest = require('./balanced-request');
+  balancedRequest = require('./balanced-request'),
+  sendMail = require('./send-mail'),
+  intentClassifier = require('./intent-classifier');
 
 mailin.start({
   port: 25,
   disableWebhook: true // Disable the webhook posting.
 });
 
-mailin.on('message', function (connection, data, content) {
+mailin.on('message', function(connection, data, content) {
   var sentence = data.text.split("\n")[0];
+  console.log('receied request for: ' + sentence);
+
   var first = sentence.split(" ")[0];
   if (first != 'note') {
-    balancedRequest(sentence, function (err, res) {
+    var command = intentClassifier(first);
+    if (command == 'get') {
+      sentence = sentence.replace('get', 'mail');
+    }
+    balancedRequest(sentence, function(err, res) {
       if (err) console.log(err);
-      if (res) console.log(res);
+      if (res) console.log(res.body);
     });
   }
 });
