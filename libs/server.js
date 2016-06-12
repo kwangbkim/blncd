91bbc21cd1fc2b7807661d2de463abd85824c41a
Kwang Kim <kkim@ondeck.com>
2016-06-12 15:35:38 -0400

    remove unused endpoints

--- libs/server.js
+++ libs/server.js
@@ -1,25 +1,22 @@
-var express = require('express'),
-  mongoose = require('mongoose'),
-  bodyParser = require('body-parser'),
-  props = require('./properties'),
-  freeFormRequest = require('./free-form'),
-  repository = require('./tasks-repository'),
-  path = require('path');
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var props = require('./properties');
+var freeFormRequest = require('./free-form');
+var repository = require('./tasks-repository');
+var path = require('path');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, '/../public'));
+
 app.use(express.static(__dirname + '/../public'));
 
 mongoose.connect(props.get("mongo:url")
   .replace('{BALANCED_DB_PASSWORD}', props.get('BALANCED_DB_PASSWORD'))
   .replace('{BALANCED_DB_USER}', props.get('BALANCED_DB_USER')));
 
-app.get('/', function(req, res) {
-  res.render('index');
-});
-
-app.get("/tasks", function(req, res) {
+app.get("/", function(req, res) {
   console.log('retrieving tasks');
   var f = function(task, quadrant) {
     return task.quadrant == quadrant;
@@ -28,27 +25,19 @@ app.get("/tasks", function(req, res) {
   repository.getAllTasks(function(err, tasks) {
     if (err) console.log(err);
     res.render('tasks', {
-      tasks1: tasks.filter(function (t) {
+      tasks1: tasks.filter(function(t) {
         return f(t, 1);
       }) || [],
-      tasks2: tasks.filter(function (t) {
+      tasks2: tasks.filter(function(t) {
         return f(t, 2);
       }) || [],
-      tasks3: tasks.filter(function (t) {
+      tasks3: tasks.filter(function(t) {
         return f(t, 3);
       }) || [],
-      tasks4: tasks.filter(function (t) {
+      tasks4: tasks.filter(function(t) {
         return f(t, 4);
       }) || []
     });
-  });
-});
-
-app.get("/tasks/:type", function(req, res) {
-  console.log('get tasks by type: ' + req.params.type);
-  repository.getTasksByType(req.params.type, function(err, tasks) {
-    if (err) console.log(err);
-    res.send(tasks);
   });
 });
 
@@ -57,31 +46,6 @@ app.post("/tasks", bodyParser.json(), function(req, res) {
   repository.insert(req.body.type, req.body.description, req.body.quadrant, function(err) {
     if (err) console.log(err);
     res.status(201).send();
-  });
-});
-
-app.get("/quadrants/:num", function(req, res) {
-  console.log('get tasks by quadrant: ' + req.params.num);
-  repository.getTasksByQuadrant(req.params.num, function(err, tasks) {
-    if (err) console.log(err);
-    res.send(tasks);
-  });
-});
-
-app.delete('/tasks/type/:type', function(req, res) {
-  console.log('delete tasks of type: ' + req.params.type);
-  repository.deleteByType(req.params.type, function(err) {
-    if (err) console.log(err);
-    res.redirect('/tasks');
-  });
-});
-
-app.post('/tasks/:id', function(req, res) {
-  console.log('delete single: ' + req.params.id);
-  repository.deleteSingle(req.params.id, function(err, task) {
-    if (err) console.log(err);
-    console.log('deleted task ', task._id)
-    res.redirect('/tasks');
   });
 });
 
