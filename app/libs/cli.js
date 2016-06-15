5aac00be6444ce354191a8c0a20999e978f77efb
Kwang Kim <kkim@ondeck.com>
2016-06-15 19:51:29 -0400

    update cli printing

--- app/libs/cli.js
+++ app/libs/cli.js
@@ -1,24 +1,54 @@
 var request = require('./balanced-request');
+var props = require('./properties');
 
-var ask = process.argv.slice(3).join(' ');
-var key = process.argv[2];
+var ask = process.argv.slice(2).join(' ');
 
-request(key, ask, function(err, res) {
+request(props.get('BLNCD_API_KEY'), ask, function(err, res) {
+	newline();
 	if (err) console.log(err);
 
 	if (ask.indexOf('get') >= 0) {
-		console.log('\n--- tasks ---');
+		var quadrant;
+		var filter = function(task) {
+			return task.quadrant == quadrant;
+		}
+
 		var tasks = [];
-		
 		for (var i in res.body) {
 			var task = res.body[i];
-			tasks.push(task.description);
+			tasks.push(task);
 		}
-		tasks.map(function(item) {
-			console.log(item);
-		});
-		console.log('--- ---\n');
+
+		for (quadrant = 1; quadrant <= 4; quadrant++) {
+			var t = tasks.filter(filter);
+			if (t.length > 0) {
+				printQuadrant(quadrant);
+				printTasks(t);
+				newline();
+			}
+		}
 	} else if (!err) {
 		console.log("ok");
 	}
 });
+
+function printQuadrant(quadrant) {
+	if (quadrant == 1)
+		console.log("-------- Important & Urgent --------");
+	if (quadrant == 2)
+		console.log("-------- Important & Not Urgent --------");
+	if (quadrant == 3)
+		console.log("-------- Not Important & Urgent --------");
+	if (quadrant == 4)
+		console.log("-------- Not Imporant & Not Urgent --------");
+}
+
+function printTasks(tasks) {
+	tasks.map(function(item) {
+		console.log(item.description);
+	});
+}
+
+function newline() {
+	console.log("");
+}
