4d7a23fc1bb3830f780efa546044f6b402cc71b7
Kwang Kim <kkim@ondeck.com>
2016-06-17 16:44:21 -0400

    send email by user api key

--- app/libs/send-mail.js
+++ app/libs/send-mail.js
@@ -1,20 +1,28 @@
-var props = require('./properties'),
-  repository = require('./tasks-repository'),
-  jsrender = require('jsrender') ,
-  sendgrid = require('sendgrid')(props.get('SENDGRID'));
+var props = require('./properties');
+var repository = require('./tasks-repository');
+var jsrender = require('jsrender');
+var sendgrid = require('sendgrid')(props.get('SENDGRID'));
+var usersRepository = require('./users-repository');
 
 function mail(key, description, callback) {
   var send = function(err, tasks) {
-    if (tasks) {
-      sendgrid.send({
-        to: props.get('user:email'),
-        from: props.get('BALANCED_SERVER_EMAIL'),
-        subject: "Balanced",
-        html: jsrender.renderFile('./templates/email-template.html', tasks)
-      }, function(err, json) {
-        callback(err, json);
-      });
-    }
+    usersRepository.getByKey(key, function(err, user) {
+      if (err) callback(err, user);
+      if (!user) {
+        callback("no user found for key:" + key, null);
+      } else {
+        if (tasks) {
+          sendgrid.send({
+            to: user.email,
+            from: props.get('BALANCED_SERVER_EMAIL'),
+            subject: "Balanced",
+            html: jsrender.renderFile('./templates/email-template.html', tasks)
+          }, function(err, json) {
+            callback(err, json);
+          });
+        }
+      }
+    });
   };
 
   var type = description.split(' ')[1];
