9b09202c6cdbf1b656ed68ee9aa84991a6536251
Kwang Kim <kkim@ondeck.com>
2016-06-18 15:53:02 -0400

    made cli a single module.  added help section

--- app/libs/cli.js
+++ app/libs/cli.js
@@ -1,20 +1,18 @@
-var blncdRequest = require('./balanced-request');
 var request = require('request');
-var props = require('./properties');
-var sf = require('./string-format');
 var readline = require('readline');
 
-sf.init();
-
 var ask = process.argv.slice(2).join(' ');
+var apiKey = process.env.BLNCD_API_KEY;
+if (!apiKey)
+	console.log("\nCan't find an api key, let's get you set up!");
 
-if (ask == 'create') {
+if (ask == 'create' || !apiKey) {
 	const rl = readline.createInterface({
 		input: process.stdin,
 		output: process.stdout
 	});
 
-	rl.question('Email integration is optional.  Type it here or just enter to skip: ', function(answer) {
+	rl.question('\nEmail integration is optional.\nType it here or just enter to skip: ', function(answer) {
 		var headers = {
 			'Content-Type': 'application/json'
 		};
@@ -41,12 +39,45 @@ if (ask == 'create') {
 				console.log("Api key created.  Create the below environment variable and you're all set:");
 				console.log("export BLNCD_API_KEY=" + body.key);
 				newline();
+				console.log("For a how-to just type: blncd help");
+				newline();
 			}
 			rl.close();
 		});
 	});
+} else if (ask == 'help') {
+	newline();
+	console.log("* Get the current list of tasks.");
+	console.log("blncd get");
+
+	newline();
+	console.log("* Here's how to add stuff.  Keywords [add | new] will both work.");
+	console.log("blncd add buy medicine is important & urgent 1");
+	console.log("blncd add read a nodejs book is important but not urgent 2");
+	console.log("blncd add pickup laundry is not important & urgent 3");
+	console.log("blncd add buy a basketball is not important & not urgent 4");
+	console.log("blncd add not specifying a priority defaults to not important & not urgent");
+	newline();
+
+	console.log("* Here's how to mark something done.  Blncd will do a fuzzy match to find the most appropriate item you're referring to.  Keywords [done | complete | delete] will all work.");
+	console.log("blncd done medicine");
+	console.log("blncd done nodejs")
+	newline();
+
+	console.log("* The first word in each task is noted as the tasks 'type'.  You can get all tasks of the same type by specifying it after 'get'.  You can complete multiple tasks at the same time using the 'finish[ed]' keyword.");
+	console.log("blncd add buy apples");
+	console.log("blncd add buy oranges");
+	console.log("blncd add buy toothpaste");
+	console.log("blncd add buy cereal");
+	console.log("blncd get buy");
+	console.log("blncd finish buy");
+	newline();
+
+	console.log("* Mail yourself the current task list.  Keywords [send | mail | email] will all work.");
+	console.log("blncd mail");
+	newline();
 } else {
-	blncdRequest(props.get('BLNCD_API_KEY'), ask, function(err, res) {
+	call(apiKey, ask, function(err, res) {
 		if (err) console.log(err);
 
 		if (ask.indexOf('get') >= 0) {
@@ -95,4 +126,26 @@ function printTasks(tasks) {
 
 function newline() {
 	console.log("");
+}
+
+function call(key, ask, callback) {
+	var headers = {
+		'Content-Type': 'application/json'
+	};
+
+	var url = "http://blncd.io/api/requests";
+
+	var options = {
+		url: url,
+		method: 'POST',
+		json: true,
+		body: {
+			ask: ask,
+			key: key
+		}
+	};
+
+	request(options, function(err, res, body) {
+		callback(err, res);
+	});
 }
