b16192d3555566a1bf585df29ec943f2e5271df3
Kwang Kim <kkim@ondeck.com>
2016-07-07 19:07:51 -0400

    use image for quadrants

--- public/js/index.jsx
+++ public/js/index.jsx
@@ -21,43 +21,28 @@ var Index = React.createClass({
 			<div>
 				<Header />
 				<div id="main-container" className="container-fluid">
-				<div className="row main-content">
-					<a href="/">
-						<img className="logo" src="assets/logo.png"/>
-					</a>
-					<h4 className="logo-text">Opinionated task list for developers.</h4>
-					<Hero />
-				</div>
-				<div className="row">
-					<div className="col-md-1 col-sm-1"/>
-					<div className="col-md-5 col-sm-5 col-xs-6">
-						<h4 className="feature-header">CLI + Email Integration</h4>
-						<p className="feature-description">
-							Access tasks through your terminal and email.<br/>
-							Add or complete tasks using either one.
-						</p>
+					<div className="row main-content">
+						<a href="/">
+							<img className="logo" src="assets/logo.png"/>
+						</a>
+						<h4 className="logo-text">Opinionated task list for developers.</h4>
+						<Hero />
 					</div>
-					<div className="col-md-5 col-sm-5 col-xs-6">
-						<h4 className="feature-header">Stephen Covey Time Management Grid</h4>
-						<div className="row">
-							<div className="col-md-6 col-sm-6 quadrant-grid-1">
-								<p className="feature-description">Important & Urgent<br/>(Quadrant 1)</p>
-							</div>
-							<div className="col-md-6 col-sm-6 quadrant-grid-2">
-								<p className="feature-description">Important & Not Urgent<br/>(Quadrant 2)</p>
-							</div>
+					<div className="row">
+						<div className="col-md-1 col-sm-1"/>
+						<div className="col-md-5 col-sm-5 col-xs-6">
+							<h4 className="feature-header">CLI + Email Integration</h4>
+							<p className="feature-description">
+								Access tasks through your terminal and email.<br/>
+								Add or complete tasks using either one.
+							</p>
 						</div>
-						<div className="row">
-							<div className="col-md-6 col-sm-6 quadrant-grid-3">
-								<p className="feature-description">Not Important & Urgent<br/>(Quadrant 3)</p>
-							</div>
-							<div className="col-md-6 col-sm-6 quadrant-grid-4">
-								<p className="feature-description">Not Important & Not Urgent<br/>(Quadrant 4)</p>
-							</div>
+						<div className="col-md-5 col-sm-5 col-xs-6">
+							<h4 className="feature-header">Stephen Covey Time Management Grid</h4>
+							<img src="assets/quadrants.png"/>
 						</div>
+						<div className="col-md-1 col-sm-1"/>
 					</div>
-					<div className="col-md-1 col-sm-1"/>
-				</div>
 				</div>
 			</div>
 		);
