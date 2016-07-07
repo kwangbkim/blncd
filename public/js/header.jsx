65463174f9aba806ba5d8c9213b017cb31aa0a0e
Kwang Kim <kkim@ondeck.com>
2016-07-07 11:12:12 -0400

    remove collapsing of navbar

--- public/js/header.jsx
+++ public/js/header.jsx
@@ -5,29 +5,20 @@ module.exports = React.createClass({
     return (
     	<nav className="navbar navbar-default">
 				<div className="container">
-					<div className="navbar-header">
-						<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#topnavbar" aria-expanded="false" aria-controls="navbar">
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-					</div>
-					<div className="collapse navbar-collapse" id="topnavbar">
-						<ul className="nav navbar-nav navbar-default">
-							<li>
-								<a id="nav-link" href="install.html">Install</a>
-							</li>
-							<li>
-								<a id="nav-link" href="usage.html">Usage</a>
-							</li>
-							<li>
-								<a id="nav-link" href="api.html">API</a>
-							</li>
-							<li>
-								<a id="nav-link" href="https://github.com/kwangbkim/balanced">Code</a>
-							</li>
-						</ul>
-					</div>
+					<ul className="nav navbar-nav">
+						<li>
+							<a id="nav-link" href="install.html">Install</a>
+						</li>
+						<li>
+							<a id="nav-link" href="usage.html">Usage</a>
+						</li>
+						<li>
+							<a id="nav-link" href="api.html">API</a>
+						</li>
+						<li>
+							<a id="nav-link" href="https://github.com/kwangbkim/balanced">Code</a>
+						</li>
+					</ul>
 				</div>
 			</nav>
     );
