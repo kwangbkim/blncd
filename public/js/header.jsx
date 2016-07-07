f3d1aab3cd1c874e18e9b42baee4c75a50ce1aed
Kwang Kim <kkim@ondeck.com>
2016-07-07 12:51:49 -0400

    properly centered navbar menu

--- public/js/header.jsx
+++ public/js/header.jsx
@@ -3,22 +3,14 @@ var React = require('react');
 module.exports = React.createClass({
   render: function() {
     return (
-    	<nav className="navbar navbar-default">
+    	<nav className="navbar">
 				<div className="container">
-					<ul className="nav navbar-nav">
-						<li>
-							<a id="nav-link" href="install.html">Install</a>
-						</li>
-						<li>
-							<a id="nav-link" href="usage.html">Usage</a>
-						</li>
-						<li>
-							<a id="nav-link" href="api.html">API</a>
-						</li>
-						<li>
-							<a id="nav-link" href="https://github.com/kwangbkim/balanced">Code</a>
-						</li>
-					</ul>
+					<div className="text-center">
+						<a className="nav-link" href="install.html">Install</a>
+						<a className="nav-link" href="usage.html">Usage</a>
+						<a className="nav-link" href="api.html">API</a>
+						<a className="nav-link" href="https://github.com/kwangbkim/balanced">Code</a>
+					</div>
 				</div>
 			</nav>
     );
