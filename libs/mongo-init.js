238284e06dc7b3ac9aad616a00f74768227158c8
Kwang Kim <kkim@ondeck.com>
2016-07-20 13:07:54 -0400

    added connection pool option to mongoose

--- libs/mongo-init.js
+++ libs/mongo-init.js
@@ -2,10 +2,16 @@ const props = require('./properties');
 const mongoose = require('mongoose');
 
 let mongoUrl = props.get("mongo:url")
-  .replace('{BLNCD_DB_PASSWORD}', props.get('BLNCD_DB_PASSWORD'))
-  .replace('{BLNCD_DB_USER}', props.get('BLNCD_DB_USER'))
-  .replace('{BLNCD_DB_URL}', props.get('BLNCD_DB_URL'));
+	.replace('{BLNCD_DB_PASSWORD}', props.get('BLNCD_DB_PASSWORD'))
+	.replace('{BLNCD_DB_USER}', props.get('BLNCD_DB_USER'))
+	.replace('{BLNCD_DB_URL}', props.get('BLNCD_DB_URL'));
 if (props.get('BLNCD_DB_REPLICA'))
-  mongoUrl = mongoUrl + "?replicaSet=" + props.get('BLNCD_DB_REPLICA');
+	mongoUrl = mongoUrl + "?replicaSet=" + props.get('BLNCD_DB_REPLICA');
 
-mongoose.connect(mongoUrl);
+const options = {
+	replset: {
+		poolSize: 5
+	}
+};
+
+mongoose.connect(mongoUrl, options);
